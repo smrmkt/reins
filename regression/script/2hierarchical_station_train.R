925106b5c9011252b724e00c3bbdc9cc6a4147cd
smrmkt <smrmkt@gmail.com>
2014-10-05 14:19:04 +0900

    add 2hierarchcal model

---
+++ regression/script/2hierarchical_station_train.R
@@ -0,0 +1,75 @@
+# load library
+library('ggplot2')
+library('doParallel')
+library('foreach')
+library('rstan')
+library('dplyr')
+
+# load data
+d = read.delim('data/mantions.csv', header=T, sep=',')
+d = na.omit(d)
+attach(d)
+st = read.delim('data/station_train.csv', header=F, sep=',')
+
+# package data for stan
+X  = t(rbind(distance, from, room, space))
+ST = st
+S  = station
+Y  = price
+d.stan = list(N=nrow(X),
+              N_T=length(unique(train)),
+              N_S=length(unique(station)),
+              M=ncol(X),
+              X=X,
+              ST=ST,
+              S=S,
+              Y=Y)
+
+# test procesing
+if (0) {
+  model.fit<-stan(file="script/2hierarchical_station_train.stan",
+                  data=d.stan,
+                  iter=40,
+                  chains=2)
+}
+
+# parallel processing
+N.chain = 3
+cl = makeCluster(N.chain)
+registerDoParallel(cl)
+sflist = foreach(i=1:N.chain, .packages='rstan') %dopar% {
+  stan(
+    file='script/2hierarchical_station_train.stan',
+    data=d.stan, iter=2000, thin=3,
+    chains=1, chain_id=i, refresh=-1
+  )
+}
+model.fit <- sflist2stanfit(sflist)
+stopCluster(cl)
+
+# save data
+save.image("output/2hierarchical_station_train/result.Rdata")
+## get summary
+print(fit, digits_summary=3)
+fit.summary <- data.frame(summary(model.fit)$summary)
+write.table(fit.summary,
+            file="output/hierarchical_station_train/fit_summary.txt",
+            sep="\t",
+            quote=F,
+            col.names=NA)
+## get plot
+pdf("output/hierarchical_station_train/fit_plot.pdf", width=600/72, height=600/72)
+plot(model.fit)
+dev.off()
+## get traceplot
+pdf("output/hierarchical_station_train/fit_traceplot.pdf", width=600/72, height=600/72)
+traceplot(model.fit)
+dev.off()
+
+# extract mcmc sample
+la <- extract(model.fit, permuted = TRUE)
+N.day <- nrow(d)
+N.mcmc <- length(la$mu)
+la$mu     #=> array
+la$weight #=> matrix
+
---
+++ regression/script/2hierarchical_station_train.stan
@@ -0,0 +1,40 @@
+data {
+    int<lower=1>            N;      # sample num
+    int<lower=1>            M;      # independents' num
+    int<lower=1>            N_T;    # train num
+    int<lower=1>            N_S;    # station num
+    matrix[N, M]            X;      # independents
+    vector[N]               Y;      # dependent
+    matrix[N_S, N_T]        ST;     # station-train matrix
+    int<lower=1, upper=N_S> S[N];   # station
+}
+parameters {
+    real             a;
+    vector[M]        b;
+    real             as;
+    vector[N_T]      r_t;
+    real             r_s[N_S];
+    real<lower=0>    s;
+    real<lower=0>    s_rs;
+    real<lower=0>    s_rt;
+}
+model {
+    # regresion model with random effect
+    for (i in 1:N)
+        Y[i] ~ normal(a+X[i]*b+r_s[S[i]], s);
+    # prior distributions
+    s ~ uniform(0, 1.0e+4);
+    a ~ normal(39, 1.0e+4);
+    for (i in 1:M)
+        b[i] ~ normal(0, 1.0e+4);
+    for (i in 1:N_S)
+        r_s[i] ~ normal(as+ST[i]*r_t, s_rs);
+    # hierarchical prior distribution
+    s_rs ~ uniform(0, 1.0e+4);
+    as   ~ normal(0, 1.0e+4);
+    for (i in 1:N_T)
+        r_t[i] ~ normal(0, s_rt);
+    # 2 hierarchical prior distibution
+    s_rt ~ uniform(0, 1.0e+4);
+}
+
